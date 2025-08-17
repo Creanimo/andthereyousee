72a440e1ea0dfe1a237bd07b4fd5fe3b4c50482a
Ferdinand Engländer <ferdinand@creanimo.net>
2025-08-17 19:41:44 +0200

    main app sketch

--- src/app.js
+++ src/app.js
@@ -1,3 +1,43 @@
 import AppScreen from "./view/AppScreen.js";
 
+switch(document.location.pathname) {
+    case "/index.html":
+        // codeblock
+        break;
+    case "/category.html":
+        // codeblock
+        break;
+    case "/entry.html":
+        // codeblock
+        break;
+}
 
+header =
+
+promptCategories = new PromptCategoryCollection({
+    dataSource: "data/promptCategories.json"
+});
+
+categoryList = new AppScreenComponent({
+    name: "categoryList",
+    template: "templates/categorySelect.mustache",
+    templateSlots: [],
+    templateFields: [],
+    data: promptCategories.mapToAppScreenComponent,
+});
+categoryScreen = new AppScreen({
+    name: "categoryScreen",
+    content: categoryList,
+});
+
+routing = [
+    ["./index.html", categoryScreen],
+    ["./categories.html", categoryScreen],
+    ["./entry.html", entryScreen]
+];
+
+app = new App({
+    routing: routing
+});
+
+app.start();
