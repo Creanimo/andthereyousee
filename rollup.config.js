a9a844d1101c7d8987f396f15b3d92ebfbe99373
Ferdinand Engländer <ferdinand@creanimo.net>
2025-08-17 14:53:18 +0200

    rollup and build conf

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  input: "./src/index.js",
+  output: [
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    nodeResolve(), 
+    commonjs(), 
+  ],
+};
