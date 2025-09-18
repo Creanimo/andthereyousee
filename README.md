f0a8cb78edd3c2c44806df1bf3834ff1f7b3da51
Ferdinand Engländer <ferdinand@creanimo.net>
2025-09-18 22:32:42 +0200

    static renderer refactor build from json

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# andthenyousee
+# Prompt Practice App
+
+## Build
+
+### Static Pages
+
+To generate the static html pages, a data folder needs to include a `src/data/pageStructure.json`.
+
+This builds the pages with logging enabled executed from the root directory:
+```bash
+ROARR_LOG=true node src/view/NodeStaticHtmlRenderer.js | npx roarr pretty-print
+```
--- src/view/NodeStaticHtmlRenderer.js
+++ src/view/NodeStaticHtmlRenderer.js
@@ -2,7 +2,9 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import Mustache from "mustache";
-import Layout from "./Layout";
+import {
+  Roarr as log,
+} from 'roarr';
 
 async function main() {
     const __filename = fileURLToPath(import.meta.url);
@@ -10,6 +12,12 @@ async function main() {
     const sourceDataDir = path.join(__dirname, "..", "data");
     const outDir = path.join(__dirname, "..", "..", "dist");
 
-    const
-    
+    const pageStructureBlueprintPath = path.join(sourceDataDir, "pageStructure.json");    
+    const pageStructureBlueprintData = await fs.readFile(pageStructureBlueprintPath, "utf8");
+    const pageStructureBlueprint = JSON.parse(pageStructureBlueprintData)
+    log.info(pageStructureBlueprint);
 }
+main().catch((err) => {
+  log.error(err);
+  process.exitCode = 1;
+});
