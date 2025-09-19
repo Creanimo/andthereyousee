644e02262c80a90a2965ce2639dbbf20fe1efd98
Ferdinand Engländer <ferdinand@creanimo.net>
2025-09-19 11:43:32 +0200

    static site generator assemble menu

--- src/view/NodeStaticHtmlRenderer.js
+++ src/view/NodeStaticHtmlRenderer.js
@@ -3,7 +3,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import Mustache from "mustache";
 import {
-  Roarr as log,
+    Roarr as log,
 } from 'roarr';
 
 async function main() {
@@ -12,12 +12,41 @@ async function main() {
     const sourceDataDir = path.join(__dirname, "..", "data");
     const outDir = path.join(__dirname, "..", "..", "dist");
 
-    const pageStructureBlueprintPath = path.join(sourceDataDir, "pageStructure.json");    
+    const pageStructureBlueprintPath = path.join(sourceDataDir, "pageStructure.json");
     const pageStructureBlueprintData = await fs.readFile(pageStructureBlueprintPath, "utf8");
     const pageStructureBlueprint = JSON.parse(pageStructureBlueprintData)
-    log.info(pageStructureBlueprint);
+
+    let menuData = { menuEntries: [] };
+    for (const page of pageStructureBlueprint.pages) {
+        if (page.showInMainMenu) {
+            menuData.menuEntries.push({ label: page.name, link: page.path })
+        }
+    }
+    log.info(menuData, "Main menu items extracted from pages.");
+
+    const menuPath = path.join(sourceDataDir, pageStructureBlueprint.layoutElements.mainMenu);
+    const layoutPath = path.join(sourceDataDir, pageStructureBlueprint.layoutElements.mainLayout)
+    const [layoutTpl, menuTpl] = await Promise.all([
+        fs.readFile(layoutPath, "utf8"),
+        fs.readFile(menuPath, "utf8"),
+    ]);
+
+    for (const page of pageStructureBlueprint.pages) {
+        log.info(page);
+        const contentData = {
+            ...menuData,
+            mainContent: '<h1>placeholder</h1>',
+        }
+        const html = Mustache.render(
+            layoutTpl, contentData,
+            {
+                mainMenu: menuTpl,
+            }
+        )
+        log.info("Here:" + html);
+    }
 }
 main().catch((err) => {
-  log.error(err);
-  process.exitCode = 1;
+    log.error(err.toString());
+    process.exitCode = 1;
 });
